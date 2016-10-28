4c8ecf7927ce07cb660b6d94098831b25618f740
lamemebete <info@lamemebete.xyz>
2016-10-28 15:46:28 +0200

    COMMIT 3 28/10/1

--- tp-7.py
+++ tp-7.py
@@ -1,3 +1,4 @@
+import re
 #7.8
 def chargement_fichier(nom_fichier):
 	L1 = []
@@ -5,7 +6,7 @@ def chargement_fichier(nom_fichier):
 	with open(nom_fichier, 'r') as f:
 		L1 = f.readlines()
 	for ligne in L1:
-		if(ligne and lign[-1] == '\n'):
+		if(ligne and ligne[-1] == '\n'):
 			L2.append(ligne[:-1])
 		else:
 			L2.append(ligne)
@@ -17,10 +18,26 @@ def sauvegarde_fichier(nom_fichier, Contenu):
 			f.write('\n')
 	return None
 sauvegarde_fichier('haiku.txt', 'Papillon voltige\nDans un monde\nSans espoir.\n(Kobayashi Issa)')
-
-def decoupage_mots(str):
-	index  = 0;
-	while(index<len(str)):
-		if(str[index])
-
+chargement_fichier('haiku.txt')
+def decoupage_mots(string):
+	wordList = string.split()
+	return wordList
+def lecture_produit(string):
+	return tuple(decoupage_mots(string))
+def lecture_commande(L):
+	listToReturn = []
+	index = 0
+	print(len(L))
+	while index < len(L):
+		listToReturn.append(lecture_produit(L[index]))
+		index += 1
+	return listToReturn
+def gen_facture(L):
+	listToRet = []
+	listToRet.append('Produit Prix')
+	listToRet.append('_______ ____')
+	index = 0
+	while index < len(L):
+		tmpVar = L[index]
+		index += 1
 
